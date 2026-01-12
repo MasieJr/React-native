e7807791ab7c0da35e541d722af830fd09eb9e3a
MasieJr <dev@masieseremu.co.za>
2026-01-12 22:27:12 +0200

    feat: enhance Pokémon display with images and type styling in the index component

--- pokedex/app/index.tsx
+++ pokedex/app/index.tsx
@@ -1,14 +1,45 @@
 import { useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
 interface Pokemon {
   name: string;
-  url: string;
+  image: string;
+  imageBack: string;
+  types: PokemonType[];
 }
+
+interface PokemonType {
+  type: {
+    name: String;
+    url: String;
+  };
+}
+
+const colorByType = {
+  grass: "#78C850",
+  fire: "#F08030",
+  water: "#6890F0",
+  bug: "#A8B820",
+  normal: "#A8A878",
+  poison: "#A040A0",
+  electric: "#F8D030",
+  ground: "#E0C068",
+  fairy: "#EE99AC",
+  fighting: "#C03028",
+  psychic: "#F85888",
+  rock: "#B8A038",
+  ghost: "#705898",
+  ice: "#98D8D8",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  flying: "#A890F0",
+};
 
 export default function Index() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
+  console.log(JSON.stringify(pokemons[0], null, 2));
   useEffect(() => {
     //fetch pokemons
     fetchPokemons();
@@ -20,18 +51,60 @@ export default function Index() {
         "https://pokeapi.co/api/v2/pokemon?limit=20"
       );
       const data = await response.json();
-      setPokemons(data.results);
+
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemon: any) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+
+          return {
+            name: pokemon.name,
+            image: details.sprites.front_default,
+            imageBack: details.sprites.back_default,
+            types: details.types,
+          };
+        })
+      );
+
+      setPokemons(detailedPokemons);
     } catch (error) {
       console.log(error);
     }
   }
   return (
-    <ScrollView>
+    <ScrollView contentContainerStyle={{ gap: 16, padding: 16 }}>
       {pokemons.map((pokemon) => (
-        <View key={pokemon.name}>
-          <Text>{pokemon.name}</Text>
+        <View
+          key={pokemon.name}
+          // @ts-ignore
+          style={{ backgroundColor: colorByType[pokemon.types[0].type.name] }}
+        >
+          <Text style={styles.name}>{pokemon.name}</Text>
+          <Text style={styles.type}>{pokemon.types[0].type.name}</Text>
+          <View style={{ flexDirection: "row" }}>
+            <Image
+              source={{ uri: pokemon.image }}
+              style={{ width: 150, height: 150 }}
+            />
+            <Image
+              source={{ uri: pokemon.image }}
+              style={{ width: 150, height: 150 }}
+            />
+          </View>
         </View>
       ))}
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  name: {
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+  type: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "grey",
+  },
+});
